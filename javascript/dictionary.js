c230f0ca046b6b8ecf3d179e23ad5524d0b4376d
fsodem <40191050+fsodem@users.noreply.github.com>
2018-06-20 15:17:19 -0700

    Day 3 commit
    Continuing my website and learning basic JavaScript

---
+++ javascript/dictionary.js
@@ -0,0 +1,19 @@
+function showDef1() {
+  const definition = "This creature can't be blocked except by creatures with flying or reach. Explained: Only other creatures with flying or with reach can block this during combat. It is effectively unblockable to those whithout flying or reach, and can block creatures with flying."
+  document.getElementById("definition").innerHTML = definition;
+}
+
+function showDef2() {
+  const definition = "Here is my definition for Def 2."
+  document.getElementById("definition").innerHTML = definition;
+}
+
+function showDef3() {
+  const definition = "Here is my definition for Def 3."
+  document.getElementById("definition").innerHTML = definition;
+}
+
+function showDef4() {
+  const definition = "Here is my definition for Def 4."
+  document.getElementById("definition").innerHTML = definition;
+}

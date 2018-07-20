611eb66413fc1d7ba4e038a532119bb27ea9c5b5
fsodem <40191050+fsodem@users.noreply.github.com>
2018-07-20 10:15:10 -0700

    Commit 2 #2
    Worked on the dictionary.

--- javascript/dictionary.js
+++ javascript/dictionary.js
@@ -1,34 +1,54 @@
-function showDef1() {
-  const definition = "This creature can't be blocked except by creatures with flying or reach. So, only other creatures with flying or with reach can block this during combat. It is effectively unblockable to those whithout flying or reach, and can block creatures with flying."
+function showDef1_1() {
+  const definition = "This creature can't be blocked except by creatures with flying or reach. So, only other creatures with flying or with reach can block this during combat. It is effectively unblockable to those whithout flying or reach, and can block creatures with flying. Note: a creature with flying can block both creatures with and without flying."
   document.getElementById("definition").innerHTML = definition;
 }
 
-function showDef2() {
+function showDef1_2() {
   const definition = "If a creature with trample would deal enough damage to its blockers to destroy them, it deals the remaning damage to the defending player. For example, if my 5/5 beast with trample was blocked by your 1/1 elf, my beast would kill the elf and deal four damage to you."
   document.getElementById("definition").innerHTML = definition;
 }
 
-function showDef3() {
+function showDef1_3() {
   const definition = "Any amount of damage a creature with deathtouch would deal to a creature is enough to destroy it (the other creature). So, even if a 1/1 wih deathtouch and a 10/10 go up againt each other, they would both die. The 10/10 would naturally kill the 1/1, but the deathtouch would allow the 1/1 to kill the 10/10"
   document.getElementById("definition").innerHTML = definition;
 }
 
-function showDef4() {
+function showDef1_4() {
   const definition = "This  creature deals combat damage before creatures without first strike. Say a 3/2 creature with first strike was in combat against a 4/3 creature without first strike. What would happen is that the 3/2 would deal damage first, dealing 3 damage to the 4/3 and killing him. If the 4/3 had survived, he would have been able to attack the 3/2 with first strike, but since he died too early the creature with first strike would survive."
   document.getElementById("definition").innerHTML = definition;
 }
 
-function showDef5() {
+function showDef1_5() {
   const definition = "Any amount of damage this creature would deal causes you to gain that much life. So, if a 3/3 with lifelink dealt three damage to a player or a creature, you would gain 3 life. Note: if a 3/3 with lifelink deals damage to a 1/1, you still gain 3 life, because it's dealing three damage, even if the creature only has one toughness."
   document.getElementById("definition").innerHTML = definition;
 }
 
-function showDef6() {
+function showDef1_6() {
   const definition = "Attacking doesn't cause this creature to tap. So, a 1/1 with vigilance could attack and not have to tap. Note: other abilities can still cause this creature to tap."
   document.getElementById("definition").innerHTML = definition;
 }
 
-function showDef7() {
-  const definition = "This creature can block creatures with flying. However, it can be blocked by anything."
+function showDef1_7() {
+  const definition = "This creature can block creatures with flying. However, it can be blocked by anything. Note: Reach does not prevent a creature with reach from blocking creatures without flying."
+  document.getElementById("definition").innerHTML = definition;
+}
+
+function showDef1_8() {
+  const definition = "This creature deals both first strike and normal strike damage. So, this creature first deals damage during the first strike phase, and then can deal damage during the normal phase as well. This creature effectively deals twice as much damage, spread out over two phases."
+  document.getElementById("definition").innerHTML = definition;
+}
+
+function showDef1_9() {
+  const definition = "This creature can't be the target of spells or abilities your opponents control. Your opponents cannot cast spells or use abilities of permanents targeting the creature with hexproof. However, effects can indirectly affect it. Say my opponent cast a spell on me that made me sacrifice a creature. I could still sacrifice a creature with hexproof as the effect is indirectly targeting the creature."
+  document.getElementById("definition").innerHTML = definition;
+}
+
+function showDef1_10() {
+  const definition = "Landwalk is not itself an ability. It is the name of an ability with many variants. One for each type of basic land - Forest, Plains, Mountain, Island, Swamp. So, the name of the abilities you will see on the cards is Forestwalk, Plainswalk, Mountainwalk, Islandwalk, Swampwalk. This creature can't be blocked by if defending player controls a ______. So, if the defending player controls a land of the type mentioned in the ability's name, the creature becomes unblockable."
+  document.getElementById("definition").innerHTML = definition;
+}
+
+function showDef2_1() {
+  const definition = "Sacrificing a creature is chosing a creature you control and immediately killing it, sacrificing it. Usually, you sacrifice a creature as a cost for some benefit, or you do it because of an effect an opponent has used on you. Note: You choose which creature to sacrifice, even if an opponent used an effect which caused you to sacrifice a creature. "
   document.getElementById("definition").innerHTML = definition;
 }

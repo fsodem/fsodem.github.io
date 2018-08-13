0dec23bbc496039183c6143a5cd541b25c3362aa
fsodem <40191050+fsodem@users.noreply.github.com>
2018-08-12 17:40:06 -0700

    Commit 2 # 6

---
+++ javascript/advanced rules.js
@@ -0,0 +1,9 @@
+function variables_in_mtg_description() {
+  const description = "In MTG, things are ever-changing and some cards need to be more flexible in their mana costs or power and toughness. Enter, variables. Click to see more."
+  document.getElementById("variables_in_mtg").innerHTML = description;
+}
+
+function variables_in_mtg_basic() {
+  const description = "Variables in MTG."
+  document.getElementById("variables_in_mtg").innerHTML = description;
+}
